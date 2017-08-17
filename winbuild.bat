92ba82b6a05931b375b3d0fd8bd6d17a8d9a5672
Daniel Romero <danieljromero@users.noreply.github.com>
2017-08-17 11:49:53 -0400

    Update winbuild.bat

--- winbuild.bat
+++ winbuild.bat
@@ -1,4 +1,4 @@
-pyinstaller --onefile --clean ^
+pyinstaller --onefile --noconsole --clean ^
 	--add-binary src\alumnifinder\finder\drivers\;src\alumnifinder\finder\drivers\ ^
 	--add-data src\alumnifinder\gui\images\;src\alumnifinder\gui\images\ ^
 	--add-data src\alumnifinder\config\;src\alumnifinder\config\ ^
