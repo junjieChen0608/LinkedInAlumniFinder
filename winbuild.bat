b4d5468d6a24c4f463fd67e7b63b4652d4f8d9fb
Junjie Chen <wiiso853@gmail.com>
2017-08-17 11:45:28 -0400

    added windows build batch file

---
+++ winbuild.bat
@@ -0,0 +1,5 @@
+pyinstaller --onefile --clean ^
+	--add-binary src\alumnifinder\finder\drivers\;src\alumnifinder\finder\drivers\ ^
+	--add-data src\alumnifinder\gui\images\;src\alumnifinder\gui\images\ ^
+	--add-data src\alumnifinder\config\;src\alumnifinder\config\ ^
+	main.py
